40a8d45fdd17d22869db5e579dd661d87b60d7c7
jmkluthe <kluthej@oregonstate.edu>
2017-02-03 20:25:25 -0800

    Adding __main__.py to run project

--- changeslow.py
+++ changeslow.py
@@ -7,19 +7,20 @@ from math import inf
 
 def changeslow(V, A):
     min = inf
-    C = [0 for el in V]
+    C = [0]*len(V)
     if A in V:
         C[V.index(A)] += 1
-        return C, 1
+        return C
     for i in range(1, A):
-        to_i, to_i_len = changeslow(V, i)
-        from_i, from_i_len = changeslow(V, A - i)
-        if to_i_len + from_i_len < min:
-            min = to_i_len + from_i_len
+        to_i = changeslow(V, i)
+        from_i = changeslow(V, A - i)
+        num_coins = sum(to_i + from_i)
+        if num_coins < min:
+            min = num_coins
             C = [a + b for a, b in zip(to_i, from_i)]
-    return C, min
+    return C
 
 
-V = [1, 5, 10]
-A = 17
-print(changeslow(V, A))
+# V = [1, 5, 10]
+# A = 17
+# print(changeslow(V, A))
